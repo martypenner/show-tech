e7d4d9c56a6986d8c687bfaa96abdab60efd5037
Marty Penner <martypenner@users.noreply.github.com>
2026-07-31 13:15:03 -0400

    link assets and settings instead of copying into the build

--- scripts/build_release.sh
+++ scripts/build_release.sh
@@ -7,6 +7,6 @@ OUT_DIR="build/release"
 mkdir -p "$OUT_DIR"
 # Use -microarch:x86-64 for running on old laptops
 odin build source/main_release -out:$OUT_DIR/showtime -strict-style -vet -no-bounds-check -o:speed
-cp -RL assets $OUT_DIR
-cp settings.sjson build/release
+ln -s "$(pwd)/assets" $OUT_DIR
+ln -s "$(pwd)/settings.sjson" $OUT_DIR
 echo "Release build created in $OUT_DIR"
